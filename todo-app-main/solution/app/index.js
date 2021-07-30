9f717f3812a0873c91dcaff9d768a9da24bb2f35
Jaspreet singh <jaspreetsinghsaini099@gmail.com>
2021-07-30 23:28:22 +0530

    data get stored in local storage

--- todo-app-main/solution/app/index.js
+++ todo-app-main/solution/app/index.js
@@ -2,6 +2,34 @@ const todoInput = document.getElementById('todo-input');
 todoInput.addEventListener('keyup', async function(event) {
     if(event.keyCode === 13 && todoInput.value !== '') {
         console.log(todoInput.value);
+        saveTodo(todoInput);
         todoInput.value = '';
     }
 });
+
+function saveTodo(todo) {
+    if(!todo) return;
+
+    let todos = [];
+    if(localStorage.getItem('todos') === null) {
+        todos = [];
+    }
+    else {
+        todos = JSON.parse(localStorage.getItem('todos'))
+    }
+    let isTodoPresent = todos.filter(oldTodo => oldTodo.toString().toLowerCase() === todo.toString().toLowerCase());
+    if(isTodoPresent.lenght) {
+        let validationToast = document.getElementById('validation-toast');
+        validationToast.innerHTML = 'todo is already there';
+        validationToast.classList.add('show-toast');
+        setTimeout(function(){
+            validationToast.classList.remove('show-toast');
+            validationToast.innerHTML = '';
+        },3000);
+        return;
+    }
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+    return true;
+
+}
